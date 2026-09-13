11a24f4bd42a37e747140329cd4b12834d39e7c1
Ayotheg <haywhyshotz@gmail.com>
2026-09-13 03:48:03 +0100

    Changed words in navbar

--- src/pages/landing/Nav.jsx
+++ src/pages/landing/Nav.jsx
@@ -26,7 +26,7 @@ function Nav() {
     { label: 'See how it works', id: 'video' },
     { label: 'Explore', id: 'explore' },
     { label: 'FAQ', id: 'faq' },
-    { label: 'Behind the scenes', id: 'bts' },
+    { label: 'BTS', id: 'bts' },
   ]
 
   const scrollTo = (id) => {
